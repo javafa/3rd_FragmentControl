5828ce3726148aeb1ab6a1c14147538be7b77dfd
javafa <nigne2016@gmail.com>
2017-06-02 14:50:02 +0900

    fragment transaction

--- app/src/main/java/com/veryworks/android/fragment/MainActivity.java
+++ app/src/main/java/com/veryworks/android/fragment/MainActivity.java
@@ -12,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 프래그먼트 생성
         createFragments();
+        // 목록 프로그먼트 화면에 세팅
         addList();
     }
 
@@ -23,8 +25,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addList(){
+        /*
+          프래그먼트 화면에 넣기
+        */
+        // 1. 프래그먼트 트랜잭션 시작하기
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // 2. 화면에 프래그먼트 넣기
         transaction.add(R.id.container, list);
+        // 3. 트랜잭션 완료
         transaction.commit();
     }
 
